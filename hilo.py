a2301ad57f8c8cd6b1d7d98248d982a8908ff6cc
Chris van Halewyn <chris@rock-unique.com>
2022-11-26 21:04:55 +0000

    Git exercise.  Initial save

---
+++ hilo.py
@@ -0,0 +1,27 @@
+from random import random
+
+x = int(round(random() * 99) + 1)
+
+print(x)
+
+guess_num = 1
+
+y = int(input(f"Enter guess {guess_num}: "))
+
+print(y)
+
+while guess_num < 4:
+    guess_num += 1
+    if x > y:
+        y = int(input(f"too low, try again, guess {guess_num}:"))
+        continue
+
+    if x < y:
+        y = int(input(f"too high, try again, guess {guess_num}:"))
+        continue
+
+    if x == y:
+        print(f"pure genius, you did it in  {guess_num-1} tries")
+        break
+
+print("too slow....")
